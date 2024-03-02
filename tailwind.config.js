8d12e897cd8bb682e32c9c642d3dbfd5a4d491a4
ayomide <mikaheelayomide5@gmail.com>
2024-03-02 11:23:04 +0100

    commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./dist/index.html',
+            './dist/index.js',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
